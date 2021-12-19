bf667dea0c4752b585fe54f9daa68a4d88103374
ashish757 <rajswns3@gmail.com>
2021-12-20 03:30:16 +0530

    v1.1.2

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -7,33 +7,6 @@ import './styles.css'
 const Home: React.FunctionComponent = () => {
   // const { dispatch } = useContext(globalContext)
   
-  //  useEffect(() => {
-    //  dispatch({type: "LOAD_NOTES"})
-  //  eslint-disable-next-line react-hooks/exhaustive-deps
-  //  }, [])
-
-
-
-  // useEffect(() => {
-
-  //   if (sessionStorage.getItem("notes")) {
-  //     let data: any = sessionStorage.getItem("notes")
-  //     setNotes(JSON.parse(data))
-  //   } else {
-  //     fetch(process.env.REACT_APP_API_DOMAIN + "get_notes.php").then(data => data.json()).then(res => {
-  //       setNotes(res)
-  //       sessionStorage.setItem("notes", JSON.stringify(res))
-  //     });
-  //   }
-    
-  //   const beforeUnload = () => {
-  //     sessionStorage.clear()
-  //     console.log("cleared");
-      
-  //   }
-  //   window.addEventListener("beforeunload", beforeUnload)
-
-  // }, [])
 
   console.log("HOME RENDERED");
   
